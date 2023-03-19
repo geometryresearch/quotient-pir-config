b20bd58a67f7eae8086d6783d8fc894bfe7a5630
Koh Wei Jie <contact@kohweijie.com>
2023-03-19 14:37:04 -0700

    precomputed data as G2 points from updated quotient-pir

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
-use clap::{arg, command, Parser};
-use clap_num::number_range;
-use ark_poly::{univariate::DensePolynomial, EvaluationDomain, GeneralEvaluationDomain};
-use ark_bn254::{Bn254, Fr, G1Projective};
+use ark_bn254::{Bn254, Fr, G2Affine, G2Projective};
 use ark_ec::AffineCurve;
 use ark_ff::bytes::FromBytes;
+use ark_poly::{univariate::DensePolynomial, EvaluationDomain, GeneralEvaluationDomain};
+use clap::{arg, command, Parser};
+use clap_num::number_range;
 use quotient_pir::{
     primitives::{
         compute_lagrange_basis_commitments, compute_lagrange_evaluation_proofs, compute_qs,
@@ -13,7 +13,7 @@ use quotient_pir::{
     update_keys::UpdateKey,
 };
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Eq)]
 pub enum Error {
     InvalidSk,
     InvalidLog2Capacity,
@@ -29,7 +29,7 @@ fn log_2_capacity_range(s: &str) -> Result<u8, String> {
 #[command(about = "Generates a config file for quotient-pir", long_about = None)]
 struct Cli {
     /// The powers of tau (PTAU) file containing a phase 1 trusted setup output
-    #[arg(short, long, required = true,)]
+    #[arg(short, long, required = true)]
     ptau: String,
 
     /// The capacity of the accumulator expressed in log_2 (e.g. log_2(1024) = 10)
@@ -37,7 +37,7 @@ struct Cli {
     log_2_capacity: u8,
 
     /// The path to the output file
-    #[arg(short, long, required = true,)]
+    #[arg(short, long, required = true)]
     output: String,
 }
 
@@ -46,11 +46,10 @@ fn main() {
     let table_size: usize = 2u64.pow(args.log_2_capacity as u32) as usize;
     let domain = GeneralEvaluationDomain::<Fr>::new(table_size).unwrap();
     let num_g1_points = table_size;
-    let num_g2_points = 0; //table_size;
-    let (srs, _srs_g2) =
-        ppot_rs::ptau::read(&args.ptau, num_g1_points, num_g2_points).unwrap();
+    let num_g2_points = table_size;
+    let (srs, srs_g2) = ppot_rs::ptau::read(&args.ptau, num_g1_points, num_g2_points).unwrap();
 
-    let srs_proj: Vec<G1Projective> = srs.iter().map(|t| t.into_projective()).collect();
+    let srs_g2_proj: Vec<G2Projective> = srs_g2.iter().map(|t| t.into_projective()).collect();
 
     let zh: DensePolynomial<Fr> = domain.vanishing_polynomial().into();
 
@@ -60,8 +59,8 @@ fn main() {
 
     let lagrange_basis_commitments = compute_lagrange_basis_commitments(&srs);
 
-    let vanishing_opening_cms = compute_qs::<Bn254>(&zh, &domain, &srs_proj);
-    let lagrange_opening_cms = compute_lagrange_evaluation_proofs::<Bn254>(&srs, &domain);
+    let vanishing_opening_cms = compute_qs::<G2Affine>(&zh, &domain, &srs_g2_proj);
+    let lagrange_opening_cms = compute_lagrange_evaluation_proofs::<G2Affine>(&srs_g2, &domain);
 
     let up_keys: Vec<_> = (0..domain.size())
         .map(|i| UpdateKey::<Bn254>::new(i, &lagrange_opening_cms, &vanishing_opening_cms))
@@ -70,7 +69,7 @@ fn main() {
     let config = Config {
         zero_value,
         domain,
-        lagrange_basis_commitments: lagrange_basis_commitments.clone(),
+        lagrange_basis_commitments,
         up_keys,
     };
     config.write_to_path(&args.output).unwrap();
